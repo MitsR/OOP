4bc3c23044ce78862c62005d360154dabe53015a
MitsR <111266824+MitsR@users.noreply.github.com>
2023-03-24 13:00:21 +0200

    Add files via upload

---
+++ Lab 17/Lab 17/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace Lab_17
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(590, 319);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 31);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Exit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(11, 10);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(558, 341);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(590, 30);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(107, 22);
+            this.textBox1.TabIndex = 2;
+
+            this.textBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyUp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(590, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Diagonal";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(590, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Side";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(590, 81);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(107, 22);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox2_KeyUp);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(590, 208);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(107, 31);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Location = new System.Drawing.Point(590, 172);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(107, 31);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Load";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(627, 241);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 16);
+            this.label3.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(635, 249);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 16);
+            this.label4.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(711, 360);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Lab 17";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Lab 17/Lab 17/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace Lab_17
+{
+    public partial class Form1 : Form
+    {
+        Romb romb;
+        Graphics graphics;
+        Pen pen;
+        int xShift = 10, yShift = 10;
+        public Form1()
+        {
+            InitializeComponent();
+
+            graphics = pictureBox1.CreateGraphics();
+            pen = new Pen(Color.Black, 2);
+            romb = new Romb();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private void textBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            foreach(char i in textBox1.Text)
+            {
+                if (!Char.IsDigit(i))
+                    return;
+            }
+
+            if (textBox1.Text != "")
+            {
+                romb.diagonal = Convert.ToInt32(textBox1.Text);
+                graphics.Clear(Color.White);
+                romb.DrawRomb(graphics, pen, xShift, yShift);
+            }
+
+            
+        }
+
+        private void textBox2_KeyUp(object sender, KeyEventArgs e)
+        {
+            foreach (char i in textBox2.Text)
+            {
+                if (!Char.IsDigit(i))
+                    return;
+            }
+
+            if (textBox2.Text != "")
+            {
+                romb.side = Convert.ToInt32(textBox2.Text);
+                graphics.Clear(Color.White);
+                romb.DrawRomb(graphics, pen, xShift, yShift);
+            }
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var xmlSerializer = new XmlSerializer(typeof(Romb));
+            var fs = new FileStream("RombSave.xml", FileMode.OpenOrCreate);
+
+            xmlSerializer.Serialize(fs, romb);
+            fs.Close();
+        }
+
+        private void LoadSerializableRomb()
+        {
+            if(File.Exists("RombSave.xml"))
+            {
+                var xmlSerializer = new XmlSerializer(typeof(Romb));
+                var fs = new FileStream("RombSave.xml", FileMode.Open);
+
+                romb = (Romb)xmlSerializer.Deserialize(fs);
+                fs.Close();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            LoadSerializableRomb();
+
+            graphics.Clear(Color.White);
+
+            textBox1.Text = romb.diagonal.ToString();
+            textBox2.Text = romb.side.ToString();
+
+            romb.DrawRomb(graphics, pen, xShift, yShift);
+        }
+    }
+
+    [Serializable]
+    public class Romb
+    {
+        public float diagonal { get; set; }
+        public float side { get; set; }
+
+        public Romb()
+        {
+            diagonal = 0;
+            side = 0;
+        }
+
+        public Romb(float Diagonal = 0, float Side = 0)
+        {
+            diagonal = Diagonal;
+            side = Side;
+        }
+
+        public void DrawRomb(Graphics graphics, Pen pen, float xShift = 0, float yShift = 0)
+        {
+            if(side<=diagonal/2 || side == 0 || diagonal == 0) { return; }
+
+            float height = (float)Math.Sqrt(Math.Pow(side, 2) - Math.Pow(diagonal / 2, 2));
+
+                graphics.DrawLine(pen, diagonal / 2 + xShift, 0 + yShift, diagonal + xShift, height + yShift);
+                graphics.DrawLine(pen, diagonal + xShift, height + yShift, diagonal / 2 + xShift, 2 * height + yShift);
+                graphics.DrawLine(pen, diagonal / 2 + xShift, 2 * height + yShift, xShift, height + yShift);
+                graphics.DrawLine(pen, xShift, height + yShift, diagonal / 2 + xShift, 0 + yShift);
+  
+
+
+        }
+    }
+}
